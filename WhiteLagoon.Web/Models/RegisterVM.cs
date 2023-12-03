61289344d365b6916fe5d286d54f1fa1dd432dcb
dape <danielpetrik@outlook.cz>
2023-12-03 22:18:11 +0100

    after chapter 127 (all issues still persist)

---
+++ WhiteLagoon.Web/Models/RegisterVM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WhiteLagoon.Web.Models;
+
+public class RegisterVM
+{
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password))]
+    [Display(Name = "Confirm Password")]
+    public string ConfirmPassword { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    [Display(Name = "Phone Number")]
+    public string PhoneNumber { get; set; }
+    public string? ReturnUrl { get; set; }
+
+    public string Role { get; set; }
+    [ValidateNever]
+    public IEnumerable <SelectListItem> RoleList { get; set; }
+}
